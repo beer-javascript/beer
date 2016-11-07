001c222ad6e1ea34461fc9bfcd9133ef93ce49d6
Ryan Loos <ryan@>
2016-11-07 09:23:07 -0800

    set up search page

--- app/routes/search.js
+++ app/routes/search.js
@@ -1,5 +1,10 @@
+import config from './config/environment';
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  
+  model() {
+    return Ember.RSVP.hash({
+      beer:
+    });
+  },
 });
